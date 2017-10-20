2b7bb608907c9c3bef2387d74d1e7716f20f25e8
donghyeon <kehdgus4152@naver.com>
2017-10-20 11:41:54 +0900

    GitTest 프로젝트 커밋

---
+++ MyFirstProject/src/MyFirstClass.java
@@ -0,0 +1,14 @@
+
+public class MyFirstClass {
+	public static void main(String[] args) {
+		int i = 1;
+		short s =1;
+		byte b =1;
+		long l =1L;
+		float f = 1.1F;
+		double d = 1.1;
+		boolean bool = true;
+		char c= '중';
+		System.out.println(Long.MAX_VALUE);
+	}
+}
